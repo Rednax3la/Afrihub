6dbea10f1978b6e7ed16849db66fe03a9b721b90
Rednax3la <132368427+Rednax3la@users.noreply.github.com>
2026-02-26 00:37:01 +0300

    Create eslint.config.js

---
+++ frontend/eslint.config.js
@@ -0,0 +1,15 @@
+import js from '@eslint/js'
+import pluginVue from 'eslint-plugin-vue'
+
+export default [
+  js.configs.recommended,
+  ...pluginVue.configs['flat/recommended'],
+  {
+    rules: {
+      'vue/multi-word-component-names': 'off',
+      'vue/no-unused-vars': 'warn',
+      'no-unused-vars': 'warn',
+      'no-console': 'off',
+    },
+  },
+]
